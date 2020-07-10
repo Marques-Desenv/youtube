e7d19af3e65e1878ab905900d78a9ef4c09d4677
Marques <60702568+Marques-Desenv@users.noreply.github.com>
2020-07-10 19:47:10 -0300

    Add files via upload
    update ydl

--- ydl.py
+++ ydl.py
@@ -1,16 +1,36 @@
 from youtube_dl import YoutubeDL
-from youtube_dl import extractor
+
 #-----------------------------------------------------------------------------------------------------------------------
-from clean_data import get_sec
-from clean_data import title_keys_clean
-from clean_data import dbInsert
+# from clean_data import get_sec
+from clean_data import keys_clean
+# from clean_data import dbInsert
 #-----------------------------------------------------------------------------------------------------------------------
 href = "https://www.youtube.com/watch?v=cw0MskC4Izo"
 #-----------------------------------------------------------------------------------------------------------------------
 def youtubedl_scrape(href):
     ydl = YoutubeDL()
     content = ydl.extract_info(href, download=False)
-    return content
+
+    channel = content['uploader']
+    title = content['title']
+    title_key_words = keys_clean(title)
+    views_ = content['view_count']
+    date_ = content['upload_date']
+    likes = content['like_count']
+    deslikes = content['dislike_count']
+    comments_ = content['comment_count']
+    tags = content['tags']
+    time_duration = content['duration']
+    img_common_color = '255,255,255'
+    img_face = True
+    categories = content['categories']
+    description = content['description']
+    description_keys_words = keys_clean(description)
+
+    values = [href, channel, title, title_key_words, views_, date_, likes, deslikes, comments_, tags, time_duration,
+              img_common_color, img_face, description, description_keys_words]
+
+    return values
 
 #TESTANDO
 print(youtubedl_scrape(href))
